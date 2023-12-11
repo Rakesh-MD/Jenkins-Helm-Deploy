a825ae058a9c9c16fab345066c8b7fe851dc25ab
Rakesh-MD <141899837+Rakesh-MD@users.noreply.github.com>
2023-12-11 15:07:16 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -13,9 +13,24 @@ sudo apt install -y maven
 sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sudo systemctl status jenkins
+# sudo systemctl status jenkins
+
+# Check Jenkins service status
+jenkins_status=$(sudo systemctl status jenkins)
+
+# Check if the service is active
+if [[ $jenkins_status =~ "Active: active" ]]; then
+    echo "Jenkins is active."
+else
+    echo "Jenkins is not active. Check the status:"
+    echo "$jenkins_status"
+fi
 
 # Display initial admin password
-echo "Wait for a moment..."
-echo "Initial Admin Password:"
+echo "Wait for a moment.......!"
+echo "Initial admin Password :"
+echo "------------------------"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+echo "Successfully Installed Jenkins...!!!"
+echo "To Access Jenkins--->  http://<your-server-ip>:8080   "
